8b615699cbdd701123ca3d5975a68b808cbfa095
Carlo Cabrera <41243450+carlocabrera@users.noreply.github.com>
2019-01-29 14:02:18 -0500

    canvas2

---
+++ rackers/src/components/Canvas2.js
@@ -0,0 +1,196 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
+const TILE_SIZE = 32;
+const WIDTH = 640;
+const HEIGHT = 360;
+let CANVAS_WIDTH = 640;
+let CANVAS_HEIGHT = 360;
+let timeWhenGameStarted = Date.now();	//return time in ms
+
+let resizeCanvas = () => {
+  CANVAS_WIDTH = window.innerWidth - 4;
+  CANVAS_HEIGHT = window.innerHeight - 4;
+
+  let ratio = 16 / 9;
+  if(CANVAS_HEIGHT < CANVAS_WIDTH / ratio)
+    CANVAS_WIDTH = CANVAS_HEIGHT * ratio;
+  else
+    CANVAS_HEIGHT = CANVAS_WIDTH / ratio;
+
+  canvas.width = WIDTH;
+  canvas.height = HEIGHT;
+  ctx.font = '30px Arial';
+  ctx.mozImageSmoothingEnabled = false;	//better graphics for pixel art
+  ctx.msImageSmoothingEnabled = false;
+  ctx.imageSmoothingEnabled = false;
+
+  canvas.style.width = `${CANVAS_WIDTH}px`;
+  canvas.style.height = `${CANVAS_HEIGHT}px`;
+}
+resizeCanvas();
+
+window.addEventListener('resize',() => {
+  resizeCanvas();
+});
+
+
+let frameCount = 0;
+
+let score = 0;
+
+let paused = false;
+
+const Img = {};
+Img.player = new Image();
+Img.player.src = "img/player.png";
+Img.bat = new Image();
+Img.bat.src = 'img/bat.png';
+Img.bee = new Image();
+Img.bee.src = 'img/bee.png';
+Img.bullet = new Image();
+Img.bullet.src = 'img/bullet.png';
+Img.upgrade1 = new Image();
+Img.upgrade1.src = 'img/upgrade1.png';
+Img.upgrade2 = new Image();
+Img.upgrade2.src = 'img/upgrade2.png';
+
+testCollisionRectRect = ({x, width, y, height}, {x, width, y, height}) => x <= x+width
+    && x <= x+width
+    && y <= y + height
+    && y <= y + height
+
+document.onmousedown = ({which}) => {
+	if(which === 1)
+		player.pressingMouseLeft = true;
+	else
+		player.pressingMouseRight = true;
+}
+document.onmouseup = ({which}) => {
+	if(which === 1)
+		player.pressingMouseLeft = false;
+	else
+		player.pressingMouseRight = false;
+}
+document.oncontextmenu = mouse => {
+	mouse.preventDefault();
+}
+
+document.onmousemove = ({clientX, clientY}) => {
+	let mouseX = clientX - canvas.getBoundingClientRect().left;
+	let mouseY = clientY - canvas.getBoundingClientRect().top;
+
+	mouseX -= CANVAS_WIDTH/2;
+	mouseY -= CANVAS_HEIGHT/2;
+
+	player.aimAngle = Math.atan2(mouseY,mouseX) / Math.PI * 180;
+}
+
+document.onkeydown = ({keyCode}) => {
+	if(keyCode === 68)	//d
+		player.pressingRight = true;
+	else if(keyCode === 83)	//s
+		player.pressingDown = true;
+	else if(keyCode === 65) //a
+		player.pressingLeft = true;
+	else if(keyCode === 87) // w
+		player.pressingUp = true;
+
+	else if(keyCode === 80) //p
+		paused = !paused;
+}
+
+document.onkeyup = ({keyCode}) => {
+	if(keyCode === 68)	//d
+		player.pressingRight = false;
+	else if(keyCode === 83)	//s
+		player.pressingDown = false;
+	else if(keyCode === 65) //a
+		player.pressingLeft = false;
+	else if(keyCode === 87) // w
+		player.pressingUp = false;
+}
+
+update = () => {
+	if(paused){
+		ctx.fillText('Paused',WIDTH/2,HEIGHT/2);
+		return;
+	}
+
+	ctx.clearRect(0,0,WIDTH,HEIGHT);
+	Maps.current.draw();
+	frameCount++;
+	score++;
+
+
+	Bullet.update();
+	Upgrade.update();
+	Enemy.update();
+
+	player.update();
+
+	ctx.fillText(`${player.hp} Hp`,0,30);
+	ctx.fillText(`Score: ${score}`,200,30);
+}
+
+startNewGame = () => {
+	player.hp = 10;
+	timeWhenGameStarted = Date.now();
+	frameCount = 0;
+	score = 0;
+	Enemy.list = {};
+	Upgrade.list = {};
+	Bullet.list = {};
+	Enemy.randomlyGenerate();
+	Enemy.randomlyGenerate();
+	Enemy.randomlyGenerate();
+
+}
+
+Maps = (id, imgSrc, grid) => {
+	const self = {
+		id,
+		image:new Image(),
+		width:grid[0].length * TILE_SIZE,
+		height:grid.length * TILE_SIZE,
+		grid,
+	};
+	self.image.src = imgSrc;
+
+	self.isPositionWall = ({x, y}) => {
+		const gridX = Math.floor(x / TILE_SIZE);
+		const gridY = Math.floor(y / TILE_SIZE);
+		if(gridX < 0 || gridX >= self.grid[0].length)
+			return true;
+		if(gridY < 0 || gridY >= self.grid.length)
+			return true;
+		return self.grid[gridY][gridX];
+	}
+
+	self.draw = () => {
+		const x = WIDTH/2 - player.x;
+		const y = HEIGHT/2 - player.y;
+		ctx.drawImage(self.image,0,0,self.image.width,self.image.height,x,y,self.image.width*2,self.image.height*2);
+	}
+	return self;
+}
+
+const array = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 502, 502, 502, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 502, 502, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+const array2D = [];
+for(let i = 0 ; i < 40; i++){
+	array2D[i] = [];
+	for(let j = 0 ; j < 40; j++){
+		array2D[i][j] = array[i * 40 + j];
+	}
+}
+
+Maps.current = Maps('field','img/map.png',array2D);
+
+
+
+player = Player();
+playerInventory = Inventory();
+startNewGame();
+
+setInterval(update,40);
