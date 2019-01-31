38231126e342ae8a350006a6bb48124c133cca01
Carlo Cabrera <41243450+carlocabrera@users.noreply.github.com>
2019-01-31 13:09:02 -0500

    centered form, blacked out nav buttons, added border to image

--- rackers/src/pages/login-form.js
+++ rackers/src/pages/login-form.js
@@ -1,101 +1,114 @@
-import React, { Component } from 'react'
-import { Redirect } from 'react-router-dom'
-import API from '../utils/API';
+import React, { Component } from "react";
+import { Redirect } from "react-router-dom";
+import API from "../utils/API";
 
 class LoginForm extends Component {
-    state = {
-        username: '',
-        password: '',
-        redirectTo: null
+  state = {
+    username: "",
+    password: "",
+    redirectTo: null
+  };
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log("handleSubmit");
+    const formData = {
+      username: this.state.username,
+      password: this.state.password
     };
-
-
-    handleChange = event => {
-        this.setState({
-            [event.target.name]: event.target.value
-        });
-    };
-
-    handleSubmit = event => {
-        event.preventDefault()
-        console.log('handleSubmit')
-        const formData = {
-            username: this.state.username,
-            password: this.state.password,
+    console.log("login user submited = " + JSON.stringify(formData));
+    API.signInUser(formData)
+      .then(response => {
+        console.log("login response: ");
+        console.log(response);
+        if (response.status === 200) {
+          // update App.js state
+          this.props.updateUser({
+            loggedIn: true,
+            username: response.data.username
+          });
+          // update the state to redirect to home
+          this.setState({
+            redirectTo: "/"
+          });
         }
-        console.log("login user submited = " + JSON.stringify(formData))
-        API.signInUser(formData)
-            .then(response => {
-                console.log('login response: ')
-                console.log(response)
-                if (response.status === 200) {
-                    // update App.js state
-                    this.props.updateUser({
-                        loggedIn: true,
-                        username: response.data.username,
-                    })
-                    // update the state to redirect to home
-                    this.setState({
-                        redirectTo: '/'
-                    })
-                }
-            }).catch(error => {
-                console.log('login error: ')
-                console.log(error);
+      })
+      .catch(error => {
+        console.log("login error: ");
+        console.log(error);
+      });
+  };
 
-            });
-    };
-
-    render() {
-        if (this.state.redirectTo) {
-            return <Redirect to={{ pathname: this.state.redirectTo }} />
-        } else {
-            return (
-                <div className="container mainContent">
-                    <h6 className="center text-28"><i className="material-icons">check_box</i> Login</h6>
-                    <form className="form-horizontal">
-                        <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="username">Username</label>
-                            </div>
-                            <div className="col-3 col-mr-auto">
-                                <input className="form-input"
-                                    type="text"
-                                    id="username"
-                                    name="username"
-                                    placeholder="Username"
-                                    value={this.state.username}
-                                    onChange={this.handleChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group">
-                            <div className="col-1 col-ml-auto">
-                                <label className="form-label" htmlFor="password">Password: </label>
-                            </div>
-                            <div className="col-3 col-mr-auto">
-                                <input className="form-input"
-                                    placeholder="password"
-                                    type="password"
-                                    name="password"
-                                    value={this.state.password}
-                                    onChange={this.handleChange}
-                                />
-                            </div>
-                        </div>
-                        <div className="form-group ">
-                            <div className="col-7"></div>
-                            <button
-                                className="btn btn-primary col-1 col-mr-auto"
-
-                                onClick={this.handleSubmit}
-                                type="submit">Login</button>
-                        </div>
-                    </form>
+  render() {
+    if (this.state.redirectTo) {
+      return <Redirect to={{ pathname: this.state.redirectTo }} />;
+    } else {
+      return (
+        <div className="container mainContent">
+          <center>
+            <h6 className="center text-28">
+              <i className="material-icons">check_box</i> Login
+            </h6>
+          </center>
+          <center>
+            <form className="form-horizontal">
+              <div className="form-group">
+                <div className="col-1 col-ml-auto">
+                  <label className="form-label" htmlFor="username">
+                    Username
+                  </label>
                 </div>
-            )
-        }
+                <div className="col-3 col-mr-auto">
+                  <input
+                    className="form-input"
+                    type="text"
+                    id="username"
+                    name="username"
+                    placeholder="Username"
+                    value={this.state.username}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+              <div className="form-group">
+                <div className="col-1 col-ml-auto">
+                  <label className="form-label" htmlFor="password">
+                    Password:{" "}
+                  </label>
+                </div>
+                <div className="col-3 col-mr-auto">
+                  <input
+                    className="form-input"
+                    placeholder="password"
+                    type="password"
+                    name="password"
+                    value={this.state.password}
+                    onChange={this.handleChange}
+                  />
+                </div>
+              </div>
+              <div className="form-group ">
+                <div className="col-7" />
+                <button
+                  className="btn btn-primary col-1 col-mr-auto"
+                  onClick={this.handleSubmit}
+                  type="submit"
+                >
+                  Login
+                </button>
+              </div>
+            </form>
+          </center>
+        </div>
+      );
     }
-};
+  }
+}
 
-export default LoginForm
+export default LoginForm;
