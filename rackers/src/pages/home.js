a138ffa443d82a27cff6555d094f3df96a936a47
Carlo Cabrera <41243450+carlocabrera@users.noreply.github.com>
2019-01-31 21:17:06 -0500

    add containers to home.js and github link to footer

--- rackers/src/pages/home.js
+++ rackers/src/pages/home.js
@@ -30,8 +30,63 @@ class Home extends Component {
           </p>
           <HighScoreTable />
         </div>
+        <>
+      <div className="container mainContent">
+        <center>
+          <h6 className="center">Game #1</h6>
+        </center>
+        <div className="row">
+          <Link to="/game1" className="active">
+            <img
+              className="col s12 m4"
+              style={imageStyle}
+              src="./images/game1.png"
+              alt="Game 1"
+              href="/game1"
+            />
+          </Link>
+          <p className="col s12 m8">
+            Click on the image to start the game! controls are WSAD for
+            movement. Left and/or right click mouse to fire bullets. pressing
+            "P" will pause the game. Try to stay alive as long as possible!
+          </p>
+          <HighScoreTable />
+        </div>
       </div>
+      </>
+
+      <>
+      <div className="container mainContent">
+        <center>
+          <h6 className="center">Game #1</h6>
+        </center>
+        <div className="row">
+          <Link to="/game1" className="active">
+            <img
+              className="col s12 m4"
+              style={imageStyle}
+              src="./images/game1.png"
+              alt="Game 1"
+              href="/game1"
+            />
+          </Link>
+          <p className="col s12 m8">
+            Click on the image to start the game! controls are WSAD for
+            movement. Left and/or right click mouse to fire bullets. pressing
+            "P" will pause the game. Try to stay alive as long as possible!
+          </p>
+          <HighScoreTable />
+        </div>
+      </div>
+      </>
+
+      </div>
+
+      
+
+      
     );
+
   }
 }
 
