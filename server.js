86b5b20838f9a1bcaa5a9be514cbfded561b6586
matttali09 <matttaliancich09@gmail.com>
2019-02-01 10:28:31 -0500

    adjusting heroku

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
+const path = require("path");
 // const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
